9c4e47b8919ba16ea9501ad582b5f68e29b22ced
Andrew Jonatan Damanik <andrewdamanik23@gmail.com>
2024-10-14 20:56:25 +0700

    change xml files

--- PotatoApp/app/build.gradle.kts
+++ PotatoApp/app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.glide)
-    implementation(libs.retrofit2)
+    implementation(libs.retrofit)
     implementation(libs.retrofit2.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
